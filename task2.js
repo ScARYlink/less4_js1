30bdd5a9e32187daba8edc5ad3064bd2bc6ab5fb
Andrey <scarylink25@gmail.com>
2017-07-13 22:46:20 +0300

    math functions

---
+++ task2.js
@@ -0,0 +1,13 @@
+/*
+Создайте переменные a=10 и b=2. Выведите на экран их сумму, разность, произведение и частное (результат деления).
+ */
+
+var a=10;
+var b=2;
+document.write(a+b);
+document.write("<br>");
+document.write(a-b);
+document.write("<br>");
+document.write(a*b);
+document.write("<br>");
+document.write(a/b);
